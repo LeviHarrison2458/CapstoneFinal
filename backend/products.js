21136e321d196f3d77fa6f8ec43a89b5f07715b0
leviharrison2458 <leviharrison237@gmail.com>
2022-11-09 05:52:13 -0800

    first commit

---
+++ backend/products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "Paris High Top Sneaker",
+    brand: "Balenciaga",
+    desc: "Balenciaga",
+    price: 850,
+    image:
+      "https://n.nordstrommedia.com/id/sr3/bc3bcf0e-563c-48c4-a7d7-83743b64386d.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2",
+  },
+  {
+    id: 2,
+    name: "Suede Moc Toe Loafer",
+    brand: "Loro Piana",
+    desc: "Lora Piana",
+    price: 930,
+    image:
+      "https://n.nordstrommedia.com/id/sr3/14e35034-0916-4df2-afb7-95dc0d29ba6f.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2",
+  },
+  {
+    id: 3,
+    name: "Alport Check Espadrille Slip-On",
+    brand: "Burberry",
+    desc: "Burberry",
+    price: 550,
+    image:
+      "https://n.nordstrommedia.com/id/sr3/1c8c6cf9-be27-4d91-8b72-5b6ea53e9cf0.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2"
+  },
+];
+
+module.exports = products;
---
+++ frontend/src/components/CheckoutSuccess.jsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import { clearCart, getTotals } from "../slices/cartSlice";
+
+const CheckoutSuccess = () => {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    dispatch(clearCart());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(getTotals());
+  }, [cart, dispatch]);
+
+  return (
+    <Container>
+      <h2>Checkout Successful</h2>
+      <p>Congratulations we are processing your order</p>
+      <p>
+        {" "}
+        <strong>Thank you for your purchase!</strong>
+      </p>
+    </Container>
+  );
+};
+
+export default CheckoutSuccess;
+
+const Container = styled.div`
+  min-height: 80vh;
+  max-width: 800px;
+  width: 100%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h2 {
+    margin-bottom: 0.5rem;
+    color: #029e02;
+  }
+`;
